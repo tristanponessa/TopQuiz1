aae32e703892120b601520c24c9a413c885e3b79
tristanponessa <tristanponessa@hotmail.com>
2020-02-04 18:16:23 +0100

    to do add dialogue file2

--- app/src/main/java/com/example/TopQuiz/controller/MainActivity.java
+++ app/src/main/java/com/example/TopQuiz/controller/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 System.out.println("clicked");
                 System.out.println(QuestionFactory.dialogue);
+                //
             }
         });
 
